c7b4d54219194ca729a7ff31c8b2c16d549e3517
JoseAlejandro96 <34602875+JoseAlejandro96@users.noreply.github.com>
2020-11-17 22:20:42 +0100

    bug fixed i was needing add application name tag to the manifest

--- app/src/main/java/com/star/project/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/star/project/newsapp/ui/NewsViewModel.kt
@@ -3,9 +3,9 @@ package com.star.project.newsapp.ui
 import android.app.Application
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.ConnectivityManager.*
-import android.net.NetworkCapabilities.*
+import android.net.NetworkCapabilities
 import android.os.Build
+import android.provider.ContactsContract
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -48,6 +48,7 @@ class NewsViewModel(
         searchNews.postValue(Resource.Loading())
         try {
             if(hasInternetConnection()){
+
                 val response = newsRepository.searchNews(searchQuery, searchNewsPage)
                 searchNews.postValue(handleSearchNewsResponse(response))
             }else{
@@ -114,33 +115,33 @@ class NewsViewModel(
         return Resource.Error(response.message())
     }
 
+
     private fun hasInternetConnection(): Boolean{
-         val connectivityManager = getApplication<NewsApplication>().getSystemService(
-                 Context.CONNECTIVITY_SERVICE
-         ) as ConnectivityManager
+        val connectivityManager = getApplication<NewsApplication>().getSystemService(
+                Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             val activeNetwork = connectivityManager.activeNetwork?: return false
             val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork)?:return false
             return when{
-                capabilities.hasTransport(TRANSPORT_WIFI) -> true
-                capabilities.hasTransport(TRANSPORT_CELLULAR) -> true
-                capabilities.hasTransport(TRANSPORT_ETHERNET) -> true
-                else -> false
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else->false
             }
         }else{
             connectivityManager.activeNetworkInfo?.run {
                 return when(type){
-                    TYPE_WIFI -> true
-                    TYPE_MOBILE -> true
-                    TYPE_ETHERNET -> true
-                    else -> false
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else->false
                 }
             }
         }
         return false
     }
-
 //    DB Operations
 
     fun saveArticle(article: Article) = viewModelScope.launch {
